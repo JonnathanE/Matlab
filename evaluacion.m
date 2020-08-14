6d6159cc5c265dd020339098fcfbdf7298317304
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2020-08-14 08:42:07 -0500

    Examen 02: metodo runge-Kuta-4 de N funciones

--- evaluacion.m
+++ evaluacion.m
@@ -1,25 +1,22 @@
+% EXAMEN 2
 % AUTOR: Jonnathan Espinoza
 clear all
 clc
 format long
 
 % N funciones
-
 disp('=== EJERCICIO RngeKutta4 de N funciones ===')
 
 % INGRESO DE LOS DATOS
-
 a = 0;
 b = 1;
-y0 = [0,1,2];
+y0 = [0 1 0 7 0];
 N = 10;
-
 
 % OPERACIÓN
 f = @f_funcionesRepaso % DECLARO MI FUNCIÓN PRIMA
 
-[xe, ye] = f_eulerNEcuaciones(f,a, b, y0, N);
 [xk, yk] = f_rk4ordenNEcuaciones(f,a, b, y0, N);
 
-varNames = {'x','Euler', 'Runge_Kuta_4'};
-T = table(xe, ye, yk, 'VariableNames', varNames)
+varNames = {'x', 'Runge_Kuta_4'};
+T = table(xk, yk, 'VariableNames', varNames)
